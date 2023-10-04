fefc6663478953e542898424c19831d7c11450a6
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2023-10-05 02:08:00 +0530

    added the input output format

--- Mathematics/Extended_Euclidean.cpp
+++ Mathematics/Extended_Euclidean.cpp
@@ -1,33 +1,24 @@
-/*
-Extended Euclidean is used to calculate the valus of the x and y which comes by the formula :
- ===> ax + by = gcd(a, b)
- ===> x = y1;
- ===> y = x1 - (a / b) * y1;
-
- */
-#include <bits/stdc++.h>
-using namespace std;
-
 #include <iostream>
+#include <utility>
 using namespace std;
 
-// pair<int, int> solve(int a, int b)
-// {
-//     if (b == 0)
-//     {
-//         return {1, 1};
-//     }
-//     auto [x1, y1] = solve(b, a % b);
-//     int x = y1;
-//     int y = x1 - (a / b) * y1;
-//     return {x, y};
-// }
+pair<int, int> solve(int a, int b)
+{
+    if (b == 0)
+    {
+        return {1, 0}; // Base case, gcd(a, 0) = a and x = 1, y = 0
+    }
+    pair<int, int> xy = solve(b, a % b);
+    int x = xy.second;
+    int y = xy.first - (a / b) * xy.second;
+    return {x, y};
+}
 
 int main()
 {
     int a, b;
     cin >> a >> b;
-    // auto [x, y] = solve(a, b);
-    // cout << x << "," << y;
+    pair<int, int> result = solve(a, b);
+    cout << result.first << "," << result.second;
     return 0;
 }
